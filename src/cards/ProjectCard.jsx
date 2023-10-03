0e5b96cc503f3ce4f93ce4b2ddd1563f9b8bb7f6
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-10-03 18:25:10 -0300

    projects ok

--- src/cards/ProjectCard.jsx
+++ src/cards/ProjectCard.jsx
@@ -1,22 +1,39 @@
-function ProjectCard(project) {
+function ProjectCard({ project }) {
   return (
     <div>
       <div className="project-title">
-        <h3>{project.title}</h3>
+        <h3>{project.name}</h3>
       </div>
-
-      <div className="image">
+      <div className="project-description">
+        <p>
+          {project.month} {project.year}
+        </p>
+      </div>
+      <div className="project-image">
         <img src={project.image}></img>
       </div>
-      <div className="description">
+
+      <div className="project-description">
         <p>{project.description}</p>
       </div>
-      <div className="logos">
+
+      <div className="project-logos">
         <ul>
-          <li>WebPage</li>
-          <li>Demo</li>
-          <li>Github</li>
-          <li>Press</li>
+          <li>
+            <a href={project.web} target="_blank" rel="noopener noreferrer">
+              <img src="/weblogo.png"></img>
+            </a>
+          </li>
+          <li>
+            <a href={project.youtube} target="_blank" rel="noopener noreferrer">
+              <img src="/youtubelogo.png"></img>
+            </a>
+          </li>
+          <li>
+            <a href={project.github} target="_blank" rel="noopener noreferrer">
+              <img src="githublogo3.png"></img>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
