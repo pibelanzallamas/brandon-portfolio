7aa4eddce71fa78ea78a332eece338116b843b87
Brandon Castillo <brandoncastillo.09@gmail.com>
2023-12-26 13:26:19 -0300

    desktop ready

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,21 +10,20 @@ function Profile() {
         </div>
         <p className="subtitle top">bio</p>
         <p className="top profile-description">
-          I was born and raised in Argentina. I have a passion for design,
-          innovation, and exploring new fields. I appreciate the value of
-          well-crafted products.
+          I was born in Argentina. I have a passion for design and innovation. I
+          appreciate the value of well-crafted products.
         </p>
         <p className="subtitle top">technologies</p>
         <ul className="lists top">
           <li>NodeJs</li>
-          <li>React</li>
-          <li>Sequelize</li>
           <li>Express</li>
+          <li>React</li>
+          <li>Postgres</li>
+          <li>Sequelize</li>
           <li>Sass</li>
           <li>JWT</li>
           <li>Figma</li>
           <li>Visual Studio Code</li>
-          <li>Postgres</li>
         </ul>
         <Credits />
       </div>
