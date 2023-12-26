a59992ba9b2345d11ae539dff8d3018065ba599a
Brandon Castillo <brandoncastillo.09@gmail.com>
2023-12-26 12:57:45 -0300

    home fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,19 @@ function Home() {
   return (
     <div className="all">
       <div className="content">
-        <h1>brandon castillo</h1>
-        <h3 className="subtitle top">web designer</h3>
-        <div className="image top">
-          <img
-            src="/img-public/cowboy-bebop-fast.gif"
-            alt="ed-riding-bike"
-          ></img>
+        <div className="ladoA">
+          <h1>brandon castillo</h1>
+          <h3 className="subtitle top">web designer</h3>
+          <div className="image top">
+            <img
+              src="/img-public/cowboy-bebop-fast.gif"
+              alt="ed-riding-bike"
+            ></img>
+          </div>
         </div>
-        <Credits />
+        <div className="ladoB">
+          <Credits />
+        </div>
       </div>
     </div>
   );
