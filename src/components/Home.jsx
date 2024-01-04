d6ef0a05598b5d582ddaa884542a783bbeeb9398
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-01-04 11:48:35 -0300

    title fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     <div className="all">
       <div className="content">
         <h1>brandon castillo</h1>
-        <h3 className="subtitle top">web designer</h3>
+        <h3 className="subtitle top">web developer</h3>
         <div className="image top">
           <img
             src="/img-public/cowboy-bebop-fast.gif"
