28d4e4bcebdc9b9c56662682beea21584603c8b9
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-01-21 19:21:09 -0300

    hover links contact add

--- src/cards/ContactCard.jsx
+++ src/cards/ContactCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function ContactCard({ link }) {
+  const dark = useSelector((state) => state.theme.dark);
+
   return (
-    <div className="contact-links center">
+    <div className={dark ? "contact-links-dark" : "contact-links-light center"}>
       <a href={link.link} target="_blank" rel="noopener noreferrer">
         <h2>{link.name}</h2>
       </a>
