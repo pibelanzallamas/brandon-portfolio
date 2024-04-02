d7b7523c13dc4c79531738ec3f2ee4075d5138ae
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-04-02 11:13:57 -0300

    dark mode

--- src/state/theme.js
+++ src/state/theme.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    dark: false,
+    dark: true,
   },
   reducers: {
     setTheme: (state, action) => {
