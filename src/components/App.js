6a4cf14485c3307d672d2b65fe44032d1cafde9c
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-05-24 13:58:17 -0300

    analytics add it

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,12 @@ import Projects from "./Projects";
 import Videos from "./Videos";
 import Contact from "./Contact";
 import "../styles/App.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
