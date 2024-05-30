e83b4d4d27f4636513ad013cd3130119008174bf
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-05-30 18:13:45 -0300

    cv add it

--- src/cards/ContactCard.jsx
+++ src/cards/ContactCard.jsx
@@ -3,10 +3,15 @@ import { useSelector } from "react-redux";
 
 function ContactCard({ link }) {
   const dark = useSelector((state) => state.theme.dark);
+  const esp = useSelector((state) => state.lang.esp);
 
   return (
     <div className={dark ? "contact-links-dark" : "contact-links-light"}>
-      <a href={link.link} target="_blank" rel="noopener noreferrer">
+      <a
+        href={esp ? link.link : link.link2}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <p>{link.name}</p>
       </a>
     </div>
