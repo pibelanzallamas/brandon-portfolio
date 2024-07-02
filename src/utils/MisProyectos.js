84a2d45cf37d7b98147261397312c9aa5d47c210
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-07-02 11:19:56 -0300

    update hod image

--- src/utils/MisProyectos.js
+++ src/utils/MisProyectos.js
@@ -66,7 +66,7 @@ const projects = [
     year: 2023,
     month: "sep",
     author: "Brandon Castillo",
-    image: "/projects-img/hod/house-of-dev-image.png",
+    image: "/projects-img/hod/github-readme-house-of-dev.png",
     web: "https://house-of-dev-client.vercel.app",
     youtube: "https://www.youtube.com/embed/uYUc50qGQXY?si=7xFUkmvLsrWGAYIu",
     video: "https://www.youtube.com/watch?v=uYUc50qGQXY",
