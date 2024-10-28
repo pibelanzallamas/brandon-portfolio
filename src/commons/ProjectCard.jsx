8b645cada2a9f217a6c91cb7ba9e172395977948
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-10-28 11:49:03 -0300

    logos fixed

--- src/commons/ProjectCard.jsx
+++ src/commons/ProjectCard.jsx
@@ -32,7 +32,7 @@ function ProjectCard({ project }) {
           <li>
             <a href={project.web} target="_blank" rel="noopener noreferrer">
               <img
-                src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126321/web_a7eixn.png"
+                src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126328/chrome_piydtk.svg"
                 alt={"logo-web"}
               ></img>
             </a>
@@ -49,12 +49,12 @@ function ProjectCard({ project }) {
             <a href={project.github} target="_blank" rel="noopener noreferrer">
               {dark ? (
                 <img
-                  src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126353/gh-dark_zahq0e.png"
+                  src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126357/gh-light_seldpl.png"
                   alt={"logo-github"}
                 ></img>
               ) : (
                 <img
-                  src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126357/gh-light_seldpl.png"
+                  src="https://res.cloudinary.com/daynclfo8/image/upload/v1730126353/gh-dark_zahq0e.png"
                   alt={"logo-github"}
                 ></img>
               )}
