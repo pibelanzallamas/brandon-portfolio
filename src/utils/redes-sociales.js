2545f8509b07a0920697f311d6b5107ac8c6f9df
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-19 14:11:05 -0300

    links update

--- src/utils/redes-sociales.js
+++ src/utils/redes-sociales.js
@@ -11,26 +11,26 @@ const links = [
     name: "Discord",
     link: "http://discord.com/users/995051014883381318",
   },
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/castill0.666",
-  },
-  {
-    name: "TikTok",
-    link: "https://www.tiktok.com/@castill0.666",
-  },
-  {
-    name: "Twitch",
-    link: "https://www.twitch.tv/brancastillodev",
-  },
+  // {
+  //   name: "Instagram",
+  //   link: "https://www.instagram.com/castill0.666",
+  // },
+  // {
+  //   name: "TikTok",
+  //   link: "https://www.tiktok.com/@castill0.666",
+  // },
+  // {
+  //   name: "Twitch",
+  //   link: "https://www.twitch.tv/brancastillodev",
+  // },
   {
     name: "Twitter",
     link: "https://twitter.com/brancastillodev",
   },
-  {
-    name: "Youtube",
-    link: "https://www.youtube.com/channel/UCIyh4HSBy4OR5R42xPJCrzw",
-  },
+  // {
+  //   name: "Youtube",
+  //   link: "https://www.youtube.com/channel/UCIyh4HSBy4OR5R42xPJCrzw",
+  // },
   {
     name: "Codewars",
     link: "https://www.codewars.com/users/pibelanzallamas",
