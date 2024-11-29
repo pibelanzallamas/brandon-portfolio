1136d16029d5328013896f6e10106afa1067f46b
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-28 22:09:43 -0300

    videos update

--- src/utils/proyectos.js
+++ src/utils/proyectos.js
@@ -8,8 +8,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732610191/calles.png",
     web: "https://calles-construction.vercel.app/",
-    youtube: "https://www.youtube.com/embed/tPLa2s6PF-E",
-    video: "https://www.youtube.com/watch?v=tPLa2s6PF-E",
+    youtube: "https://www.youtube.com/embed/mOV8O6jPwC4",
+    video: "https://www.youtube.com/watch?v=mOV8O6jPwC4",
     github: "http://github.com/pibelanzallamas/calles-construction",
     description:
       "Web site to showcase Calle's Construction different works, from drywall, to electricity, allows to upload jobs with details and images.",
@@ -26,8 +26,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732663508/yugi.png",
     web: "https://givemeayugiohcard.vercel.app/",
-    youtube: "https://www.youtube.com/embed/ffR6Gch-64M?si=sXYb7vc3YC0FX-9z",
-    video: "https://www.youtube.com/watch?v=ffR6Gch-64M",
+    youtube: "https://www.youtube.com/embed/NEzQ7VnWCLI",
+    video: "https://www.youtube.com/watch?v=NEzQ7VnWCLI",
     github: "http://github.com/pibelanzallamas/givemeayugiohcard",
     description: "Get a Yu-Gi-Oh! card.",
     descripcion: "Obtene un carta Yu-Gi-Oh!",
@@ -42,8 +42,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732664586/mood.png",
     web: "https://calles-construction.vercel.app/",
-    youtube: "https://www.youtube.com/embed/uBFMMUuiVNg?si=eEaU3xc94zIVHRlR",
-    video: "https://www.youtube.com/watch?v=uBFMMUuiVNg",
+    youtube: "https://www.youtube.com/embed/LliCjVK8ZBQ",
+    video: "https://www.youtube.com/watch?v=LliCjVK8ZBQ",
     github: "http://github.com/pibelanzallamas/calles-construction",
     description: "Website for recommending music based on your mood.",
     descripcion: "Sitio web que te recomienda musica segun tu estado de animo.",
@@ -58,8 +58,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732665477/elder.png",
     web: "https://yourelder.vercel.app/",
-    youtube: "https://www.youtube.com/embed/Ch5kE_baIUA?si=j8ZKJAfV1SqcWlQV",
-    video: "https://www.youtube.com/watch?v=Ch5kE_baIUA",
+    youtube: "https://www.youtube.com/embed/VXR-ek3Ez50?si=j8ZKJAfV1SqcWlQV",
+    video: "https://www.youtube.com/watch?v=VXR-ek3Ez50",
     github: "http://github.com/pibelanzallamas/yourelder",
     description: "Youtube song searcher for elders.",
     descripcion: "Buscador de Youtube más simplificado.",
@@ -74,8 +74,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732568128/carbon.png",
     web: "http://carbon-copy-omega.vercel.app",
-    youtube: "https://www.youtube.com/embed/Nm8IPGoRfsI?si=h4T4HCtKldUgbeUb",
-    video: "https://www.youtube.com/watch?v=Nm8IPGoRfsI",
+    youtube: "https://www.youtube.com/embed/bb8RomFHFdg?si=h4T4HCtKldUgbeUb",
+    video: "https://www.youtube.com/watch?v=bb8RomFHFdg",
     github: "https://github.com/pibelanzallamas/carbon-copy",
     description: "Application to edit and export code to PNG images.",
     descripcion: "Aplicación para editar y exportar código a imágenes PNG.",
@@ -102,8 +102,8 @@ const projects = [
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732626769/dreamer.png",
     darkimage: "/projects-img/dp/dreamer-paintings-dark.png",
     web: "https://dreamer-paintings.vercel.app",
-    youtube: "https://www.youtube.com/embed/vVAOLCLHERQ?si=VWodVsSxDQwnyytl",
-    video: "https://www.youtube.com/watch?v=vVAOLCLHERQ",
+    youtube: "https://www.youtube.com/embed/onv0uQ7Y_1I?si=VWodVsSxDQwnyytl",
+    video: "https://www.youtube.com/watch?v=onv0uQ7Y_1I",
     github: "https://github.com/pibelanzallamas/dreamers-paintings",
     description:
       "Exploratory and informative website showcasing the paintings of a talented friend from the city of Buenos Aires.",
@@ -120,8 +120,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732568386/house.png",
     web: "https://house-of-dev-client.vercel.app",
-    youtube: "https://www.youtube.com/embed/ihdiqy8aBIU?si=wYhTO8X4uZLANW6c",
-    video: "https://www.youtube.com/watch?v=ihdiqy8aBIU",
+    youtube: "https://www.youtube.com/embed/LiaV7xHjJmk?si=wYhTO8X4uZLANW6c",
+    video: "https://www.youtube.com/watch?v=LiaV7xHjJmk",
     github: "https://github.com/pibelanzallamas/house-of-dev-client-deploy",
     description:
       "Web application for searching properties with various filters, sorting options, favorites, scheduling appointments, email confirmation, and admin mode.",
@@ -149,8 +149,8 @@ const projects = [
     image:
       "https://res.cloudinary.com/daynclfo8/image/upload/v1732567910/crib.png",
     web: "https://my-movie-crib-front.vercel.app/1",
-    youtube: "https://player.vimeo.com/video/897978371?h=3227292034",
-    video: "https://vimeo.com/897978371?share=copy",
+    youtube: "https://www.youtube.com/embed/j-qgyoaeJN4?si=wYhTO8X4uZLANW6c",
+    video: "https://www.youtube.com/watch?v=j-qgyoaeJN4",
     github: "https://github.com/pibelanzallamas/my-movie-crib-front",
     description:
       "Web application for searching movies, viewing the trending ones, saving favorites, and visiting their websites.",
@@ -168,40 +168,40 @@ const projects = [
       "jwt",
     ],
   },
-  {
-    id: 2,
-    name: "Metalhead",
-    year: 2023,
-    month: "may",
-    author: "Brandon Castillo",
-    image:
-      "https://res.cloudinary.com/daynclfo8/image/upload/v1732841668/metal.png",
-    web: "https://pibelanzallamas.github.io/metalhead",
-    youtube: "https://www.youtube.com/embed/MixyinKSZhk?si=Oo-y5EnDjF4wcmSJ",
-    video: "https://www.youtube.com/watch?v=MixyinKSZhk",
-    github: "https://github.com/pibelanzallamas/metalhead",
-    description:
-      "Little project that includes a mixtape of ps1 videogames, breakcore music and live nineties bands.",
-    descripcion:
-      "Pequeño proyecto que incluye una mezcla de videojuegos de PS1, música breakcore y bandas de los noventa",
-    tags: ["html", "css", "nodejs", "express", "postgres", "sql"],
-  },
-  {
-    id: 1,
-    name: "Sacrificial Suicide",
-    year: 2023,
-    month: "feb",
-    author: "Brandon Castillo",
-    image:
-      "https://res.cloudinary.com/daynclfo8/image/upload/v1732841819/sacri.png",
-    web: "https://pibelanzallamas.github.io/sacrificial-suicide/",
-    youtube: "https://www.youtube.com/embed/5aZPzEy0ANc?si=A7DxxHmdLj-j5wnN",
-    video: "https://www.youtube.com/watch?v=5aZPzEy0ANc",
-    github: "https://github.com/pibelanzallamas/sacrificialsuicide",
-    description: "Little project that counts your days alive.",
-    descripcion: "Pequeño proyecto que te calcula los días vivo.",
-    tags: ["html", "css", "nodejs", "express", "postgres", "sql"],
-  },
+  // {
+  //   id: 2,
+  //   name: "Metalhead",
+  //   year: 2023,
+  //   month: "may",
+  //   author: "Brandon Castillo",
+  //   image:
+  //     "https://res.cloudinary.com/daynclfo8/image/upload/v1732841668/metal.png",
+  //   web: "https://pibelanzallamas.github.io/metalhead",
+  //   youtube: "https://www.youtube.com/embed/MixyinKSZhk?si=Oo-y5EnDjF4wcmSJ",
+  //   video: "https://www.youtube.com/watch?v=MixyinKSZhk",
+  //   github: "https://github.com/pibelanzallamas/metalhead",
+  //   description:
+  //     "Little project that includes a mixtape of ps1 videogames, breakcore music and live nineties bands.",
+  //   descripcion:
+  //     "Pequeño proyecto que incluye una mezcla de videojuegos de PS1, música breakcore y bandas de los noventa",
+  //   tags: ["html", "css", "nodejs", "express", "postgres", "sql"],
+  // },
+  // {
+  //   id: 1,
+  //   name: "Sacrificial Suicide",
+  //   year: 2023,
+  //   month: "feb",
+  //   author: "Brandon Castillo",
+  //   image:
+  //     "https://res.cloudinary.com/daynclfo8/image/upload/v1732841819/sacri.png",
+  //   web: "https://pibelanzallamas.github.io/sacrificial-suicide/",
+  //   youtube: "https://www.youtube.com/embed/5aZPzEy0ANc?si=A7DxxHmdLj-j5wnN",
+  //   video: "https://www.youtube.com/watch?v=5aZPzEy0ANc",
+  //   github: "https://github.com/pibelanzallamas/sacrificialsuicide",
+  //   description: "Little project that counts your days alive.",
+  //   descripcion: "Pequeño proyecto que te calcula los días vivo.",
+  //   tags: ["html", "css", "nodejs", "express", "postgres", "sql"],
+  // },
 ];
 
 export default projects;
