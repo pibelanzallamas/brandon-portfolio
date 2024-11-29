ddcf641c9d65888164cb2317630ba0bc391ad757
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-28 22:37:18 -0300

    social networks update

--- src/utils/redes-sociales.js
+++ src/utils/redes-sociales.js
@@ -1,36 +1,8 @@
 const links = [
   {
-    name: "Github",
-    link: "https://github.com/pibelanzallamas",
-  },
-  {
     name: "LinkedIn",
     link: "https://www.linkedin.com/in/brandon-castillo-dev",
   },
-  {
-    name: "Discord",
-    link: "http://discord.com/users/995051014883381318",
-  },
-  // {
-  //   name: "Instagram",
-  //   link: "https://www.instagram.com/castill0.666",
-  // },
-  // {
-  //   name: "TikTok",
-  //   link: "https://www.tiktok.com/@castill0.666",
-  // },
-  // {
-  //   name: "Twitch",
-  //   link: "https://www.twitch.tv/brancastillodev",
-  // },
-  {
-    name: "Twitter",
-    link: "https://twitter.com/brancastillodev",
-  },
-  // {
-  //   name: "Youtube",
-  //   link: "https://www.youtube.com/channel/UCIyh4HSBy4OR5R42xPJCrzw",
-  // },
   {
     name: "Codewars",
     link: "https://www.codewars.com/users/pibelanzallamas",
