90ed67d2aefb32de5ca8f3ed50169d3947ddaeab
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-30 16:54:23 -0300

    title update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,9 @@ function Home() {
     <div className={dark ? "all all-dark" : "all all-light"}>
       <div className="content">
         <h1>brandon castillo</h1>
-        <h3 className={dark ? "subtitle-dark top" : "subtitle-light top"}>
+        <h2 className={dark ? "subtitle-dark top" : "subtitle-light top"}>
           <u>{esp ? <>desarrollador web</> : <>web developer</>}</u>
-        </h3>
+        </h2>
         <div className="image home-image top" style={{ marginTop: "2rem" }}>
           <img src={home} loading="lazy" alt="ed-riding-bike"></img>
         </div>
