ad151acaa33395e62232693911dfdea6fa1dce61
pibelanzallamas <brandoncastillo.09@gmail.com>
2025-05-07 16:00:26 -0300

    cv update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,14 +1,16 @@
-import Credits from "./Credits";
 import { useSelector } from "react-redux";
 import profile from "../assets/profile.png";
+import cv_esp from "../assets/b_c.pdf"
+import cv_eng from "../assets/b_c_r.pdf"
+
 
 function Profile() {
   const esp = useSelector((state) => state.lang.esp);
   const dark = useSelector((state) => state.theme.dark);
   const cv = {
-    link: "https://drive.google.com/file/d/1oCK1fn9D4Rs6cUda6Pd6_CWivQc7raMp/view?usp=sharing",
+    link: cv_esp,
     link2:
-      "https://drive.google.com/file/d/1qVugpyvGeV47p5JdhU1RvcIpeixqZ_p-/view?usp=sharing",
+      cv_eng
   };
 
   return (
