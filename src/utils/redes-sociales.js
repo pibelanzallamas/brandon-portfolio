de4aa4325c4a65cd6ed882a17ab09960dc2f0282
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-08-05 23:04:12 -0300

    UPDATE: redes

--- src/utils/redes-sociales.js
+++ src/utils/redes-sociales.js
@@ -4,12 +4,37 @@ const links = [
     link: "https://www.linkedin.com/in/bran-castillo-dev",
   },
   {
+    name: "Github",
+    link: "https://github.com/pibelanzallamas",
+  },
+  {
+    name: "Twitch",
+    link: "https://www.twitch.tv/brancastillodev/"
+
+  },
+  {
+    name: "Gmail",
+    link: "mailto:brancastillo.developer@gmail.com"
+  },
+  {
+    name: "Discord",
+    link: "https://discordapp.com/users/1363126156378771456"
+  },
+  {
+    name: "X",
+    link: "https://x.com/brancastillodev"
+  },
+  {
+    name: "Letterbox",
+    link: "https://letterboxd.com/pibelanzallamas/",
+  },
+  {
     name: "Codewars",
     link: "https://www.codewars.com/users/pibelanzallamas",
   },
   {
-    name: "Github",
-    link: "https://github.com/pibelanzallamas",
+    name: "Reddit",
+    link: "https://www.reddit.com/user/Emergency-Panda-18/",
   },
 ];
 
