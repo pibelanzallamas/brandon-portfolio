ef749cf13deaca43abddba92d541ed17e9e04b83
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-08-11 16:50:37 -0300

    UPDATE: redes sort

--- src/utils/redes-sociales.js
+++ src/utils/redes-sociales.js
@@ -1,20 +1,16 @@
 const links = [
-
-  {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/bran-castillo-dev",
-  },
   {
     name: "Github",
     link: "https://github.com/pibelanzallamas",
   },
   {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/bran-castillo-dev",
+  },
+  {
     name: "Twitch",
     link: "https://www.twitch.tv/brancastillodev/"
-
   },
-
-
   {
     name: "X",
     link: "https://x.com/brancastillodev"
@@ -23,7 +19,6 @@ const links = [
     name: "Discord",
     link: "https://discordapp.com/users/1363126156378771456"
   },
-
   {
     name: "Gmail",
     link: "mailto:brancastillo.developer@gmail.com"
