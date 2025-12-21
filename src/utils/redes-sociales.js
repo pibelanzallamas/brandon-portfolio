68d9fa8e1fca00053946a6fd65aa8e282bef79c1
Brandon Castillo <brandoncastillo.09@gmail.com>
2025-12-21 14:31:26 -0300

    update: contact links

--- src/utils/redes-sociales.js
+++ src/utils/redes-sociales.js
@@ -1,39 +1,23 @@
 const links = [
   {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/bran-castillo-dev",
+  },
+  {
     name: "Github",
     link: "https://github.com/pibelanzallamas",
   },
-  {
+    {
     name: "Discord",
     link: "https://discordapp.com/users/995051014883381318"
-  },
-  {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/bran-castillo-dev",
   },
   {
     name: "Codewars",
     link: "https://www.codewars.com/users/pibelanzallamas",
   },
   {
-    name: "Letterbox",
-    link: "https://letterboxd.com/pibelanzallamas/",
-  },
-  {
-    name: "Reddit",
-    link: "https://www.reddit.com/user/Emergency-Panda-18/",
-  },
-  {
-    name: "X",
-    link: "https://x.com/brancastillodev"
-  },
-  {
     name: "Gmail",
     link: "mailto:brandoncastillo.09@gmail.com"
-  },
-  {
-    name: "Twitch",
-    link: "https://www.twitch.tv/brancastillodev/"
   },
 ];
 
