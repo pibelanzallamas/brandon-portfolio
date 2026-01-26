3d65a8af688a99fe5fb21e7b78e81ee1e6551e35
Brandon Castillo <brandoncastillo.09@gmail.com>
2026-01-26 18:51:40 -0300

    new subtitle

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
     <main>
       <h1><a className="name" href="https://linkedin.com/in/brandon-castillo-dev" target="_blank">brandon castillo</a></h1>
       <h2>
-        <a className="name"  href="https://api.constata.eu/certificate/research+riding+veal+bonfire+dedicator" target="_blank">{esp ? <>Desarrollador Front End</> : <>Front End Developer</>}</a>
+        <a className="name"  href="https://api.constata.eu/certificate/research+riding+veal+bonfire+dedicator" target="_blank">{esp ? <>Front End y diseño ux</> : <>Front End & UX Designer</>}</a>
       </h2>
       <figure
         className="image home-image"
